749249625b5b41d8c006194f51c49c77a248ab6b
stian-git <82411646+stian-git@users.noreply.github.com>
2023-03-03 11:48:35 +0100

    Test

--- config/server.js
+++ config/server.js
@@ -1,10 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', 'www.tekniskpotet.no'),
-  port: env.int('PORT', 443),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: env('PUBLIC_URL', 'https://www.tekniskpotet.no/strapi'),
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
