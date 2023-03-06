0b2a08ccc91908527973f86d918fa127ec9ac6bf
stian-git <82411646+stian-git@users.noreply.github.com>
2023-03-06 08:56:32 +0100

    Locally working Strapi against railway postgres

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,20 +1,34 @@
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
+module.exports = ({ env }) => ({
     connection: {
       client: 'postgres',
       connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-        ssl: { rejectUnauthorized: false },
+        host: env('PGHOST', '127.0.0.1'),
+        port: env.int('PGPORT', 5931),
+        database: env('PGDATABASE', 'railway'),
+        user: env('PGUSER', 'postgres'),
+        password: env('PGPASSWORD', 'password'),
+        ssl: env.bool(true),
       },
-      debug: false,
     },
-  }
-};
+  });
+
+// const { parse } = require("pg-connection-string");
+
+// module.exports = ({ env }) => {
+//   const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+//   return {
+//     connection: {
+//       client: 'postgres',
+//       connection: {
+//         host,
+//         port,
+//         database,
+//         user,
+//         password,
+//         ssl: { rejectUnauthorized: false },
+//       },
+//       debug: false,
+//     },
+//   }
+// };
