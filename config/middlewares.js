cd2ad6c5fe53156e1d280bad7064250f9c141ebe
stian-git <82411646+stian-git@users.noreply.github.com>
2023-03-07 10:08:32 +0100

    Minor changes

--- config/middlewares.js
+++ config/middlewares.js
@@ -35,16 +35,3 @@ module.exports = [
   "strapi::favicon",
   "strapi::public",
 ];
-
-// module.exports = [
-//   'strapi::errors',
-//   'strapi::security',
-//   'strapi::cors',
-//   'strapi::poweredBy',
-//   'strapi::logger',
-//   'strapi::query',
-//   'strapi::body',
-//   'strapi::session',
-//   'strapi::favicon',
-//   'strapi::public',
-// ];
